866b3d6180f6e8517fe52c775ebe1fd8b24e4a28
David Balla <szefoka@gmail.com>
2017-10-07 21:45:26 +0200

    pipework added

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,4 +1,7 @@
 sudo apt-get update
 sudo apt-get install docker.io -y
 sudo apt-get install openvswitch-switch -y
+sudo apt-get install git -y
+git clone https://github.com/jpetazzo/pipework.git
+mv ./pipework/pipework /usr/bin
 sudo docker build -t lab - < Dockerfile
