9cffd895f10cc3bf1fdad35871ba97212fac1783
root <root@node1.szefoka-qv29562.cloudnet-pg0.clemson.cloudlab.us>
2017-10-17 09:49:04 -0400

    ovs-docker install added

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,7 +1,9 @@
 sudo apt-get update
 sudo apt-get install docker.io -y
 sudo apt-get install openvswitch-switch -y
-sudo apt-get install git -y
+wget https://raw.githubusercontent.com/openvswitch/ovs/master/utilities/ovs-docker
+chmod a+rwx ovs-docker
+mv ovs-docker /usr/bin
 git clone https://github.com/jpetazzo/pipework.git
 mv ./pipework/pipework /usr/bin
 sudo docker build -t lab - < Dockerfile
